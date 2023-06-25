b93f86957b2da29527c69efd94243f337671e279
PrayushiFaldu <prayushifaldu123@gmail.com>
2023-06-25 16:28:28 +0530

    Add Evaluation Code

---
+++ Code/Evaluation/eval_rng.sh
@@ -0,0 +1,15 @@
+
+python final_evaluation.py \
+     --model 'rng' \
+     --data ../GrailQAbility/grailqa_v1.0_test_a_na_reference.json \
+     --predict results/RnG-KBQA/train_A_U_threshold_RnG_model_test_A_U.txt \
+     --fb_roles ontology/fb_roles \
+     --fb_types ontology/fb_types \
+     --reverse_properties ontology/reverse_properties \
+     --partial_zero_shot_type_drop ../GrailQAbility/entity_type_deletion/test/zero-shot/partial/grailqa_v1.0_test_eType_npuua.json \
+     --complete_zero_shot_type_drop ../GrailQAbility/entity_type_deletion/test/zero-shot/full/grailqa_v1.0_test_eType_ncuua.json \
+     --partial_zero_shot_rel_drop ../GrailQAbility/relation_deletion/test/zero-shot/partial/grailqa_v1.0_test_rel_npuua.json \
+     --complete_zero_shot_rel_drop ../GrailQAbility/relation_deletion/test/zero-shot/full/grailqa_v1.0_test_rel_ncuua.json \
+
+     
+
